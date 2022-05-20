6bea5b2af47e30e7f2aae0d61ba7e2b0d1f534a2
Konrad Gnat <konradgnat@Konrads-MacBook-Pro-2.local>
2022-05-20 17:47:08 -0400

    energize particle

--- frontend/services/energize.ts
+++ frontend/services/energize.ts
@@ -12,10 +12,13 @@ export const approveUSDC = async (provider: any, amount: number) => {
     const signer = provider.getSigner();
     debugger;
 
-    const usdcContract = new ethers.Contract("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", ERC20, signer);
+    // const usd_coin = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174';
+    const usdcContract = new ethers.Contract("0x2791bca1f2de4661ed88a30c99a7a9449aa84174", ERC20, signer);
 
+    console.log('to wei ', web3.utils.toWei('1'));
+    
     // totalSupply tokenByIndex tokenURI
-    const response = await usdcContract.approve(ChargedParticles.address, 1000000);
+    const response = await usdcContract.approve(ChargedParticles.address, web3.utils.toWei("1"));
     console.log(`response: ${JSON.stringify(response)}`)
 }
 
@@ -45,7 +48,7 @@ export const energizeParticle = async (
             ProtonB.address,
             tokenId,
             walletManagerId,
-            assetToken,
+            "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
             assetAmount + '000000', // usdc is 6 decimals
             "0x0000000000000000000000000000000000000000"
         );
--- frontend/utils/globals.ts
+++ frontend/utils/globals.ts
@@ -1,1 +1,1 @@
-export const USDC_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+export const USDC_ADDRESS = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174';
