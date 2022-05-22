1beee1cea2dedfb4fdac2318c5911d733c0b3186
frag 997 <fraguilar@pm.me>
2022-05-22 03:04:56 -0300

    Evolving charts

--- frontend/components/Charts/CustomCharts/index.tsx
+++ frontend/components/Charts/CustomCharts/index.tsx
@@ -1,27 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import Charts from "../Charts";
-
-const useIsMounted = () => {
-  const isMounted = useRef(false);
-  useEffect(() => {
-    isMounted.current = true;
-    return () => {
-      isMounted.current = false;
-    };
-  }, []);
-  return isMounted;
-};
+import useIsMounted from "../../../utils/hooks/isMounted";
 
 export default function CustomCharts() {
   const isMounted = useIsMounted();
 
-  const [chartData, setChartData] = useState({
-    btc_only: [[], []],
-    eth_only: [[], []],
-    invest_50_50: [[], []],
-    invest_60_40: [[], []],
-    invest_80_20: [[], []],
-    invest_day: [[], []],
+  const [chartData, setChartData] = useState<any>({
+    btc_only: [[],[]],
+    eth_only: [[]],
+    invest_50_50: [[]],
+    invest_60_40: [[]],
+    invest_80_20: [[]],
+    invest_day: [[]],
   });
 
   const charts = [
@@ -32,48 +22,138 @@ export default function CustomCharts() {
       props: {
         unit: "$",
       },
+      type: 'line',
+    },
+    {
+      title: "ETH ONLY",
+      description: "Lorem IPSUM",
+      data: chartData.eth_only,
+      props: {
+        unit: "$",
+      },
+      type: 'line',
+    },
+    {
+      title: "50/50",
+      description: "Lorem IPSUM",
+      data: chartData.invest_50_50,
+      props: {
+        unit: "$",
+      },
+      type: 'line',
+    },
+    {
+      title: "60/40",
+      description: "Lorem IPSUM",
+      data: chartData.invest_60_40,
+      props: {
+        unit: "$",
+      },
+      type: 'line',
+    },
+    {
+      title: "80/20",
+      description: "Lorem IPSUM",
+      data: chartData.invest_80_20,
+      props: {
+        unit: "$",
+      },
+      type: 'line',
+    },
+    {
+      title: "Day",
+      description: "Lorem IPSUM",
+      data: chartData.invest_day,
+      props: {
+        unit: "$",
+      },
+      type: 'line',
     },
   ];
 
   async function loadDataCharts() {
     // TODO -> data shenaningans
-    // const data = [
-    //   {
-    //     btc_only: 171205.72,
-    //     date_time: "2021-11-14",
-    //     eth_only: 331093.38,
-    //     invest_50_50: 251149.55,
-    //     invest_60_40: 267138.32,
-    //     invest_80_20: 299115.85,
-    //     invest_day: 1049.0,
-    //   },
-    // ];
+    const MockData = [
+      {
+        btc_only: 171205.72,
+        date_time: "2021-11-14",
+        eth_only: 331093.38,
+        invest_50_50: 251149.55,
+        invest_60_40: 267138.32,
+        invest_80_20: 299115.85,
+        invest_day: 1049.0,
+      },
+      {
+        btc_only: 171205.72,
+        date_time: "2021-11-15",
+        eth_only: 331093.38,
+        invest_50_50: 251149.55,
+        invest_60_40: 267138.32,
+        invest_80_20: 299115.85,
+        invest_day: 1049.0,
+      },
+      {
+        btc_only: 171205.72,
+        date_time: "2021-11-16",
+        eth_only: 331093.38,
+        invest_50_50: 251149.55,
+        invest_60_40: 267138.32,
+        invest_80_20: 299115.85,
+        invest_day: 1049.0,
+      },
+    ];
 
-    // const btc_only = [[...data.date_time], [...data.btc_only]];
-    // const eth_only = [[...data.date_time], [...data.eth_only]];
-    // const invest_50_50 = [[...data.date_time], [...data.invest_50_50]];
-    // const invest_60_40 = [[...data.date_time], [...data.invest_60_40]];
-    // const invest_80_20 = [[...data.date_time], [...data.invest_80_20]];
-    // const invest_day = [[...data.date_time], [...data.invest_day]];
+    const getTimestamp = (date_time: String): Number => {
+      var dateParts = date_time.match(/(\d+)-(\d+)-(\d+) (\d+):(\d+)/) || "";
+      const date = new Date(
+        parseInt(dateParts[2]),
+        parseInt(dateParts[1], 10) - 1,
+        parseInt(dateParts[0])
+      );
+      return date.getTime() / 1000
+    }
 
-    // if (isMounted.current) {
-    //   setChartData({
-    //     btc_only: btc_only,
-    //     eth_only: eth_only,
-    //     invest_50_50: invest_50_50,
-    //     invest_60_40: invest_60_40,
-    //     invest_80_20: invest_80_20,
-    //     invest_day: invest_day,
-    //   });
-    // }
+      const btc_only = [
+        MockData.map((d) => {
+          return { time: (getTimestamp(d.date_time)), value: d.btc_only }}),
+        ];
+      const eth_only = [
+        MockData.map((d) => {
+          return { time: (getTimestamp(d.date_time)), value: d.eth_only }}),
+      ];
+      const invest_50_50 = [
+        MockData.map((d) => {
+          return { time: (getTimestamp(d.date_time)), value: d.invest_50_50 }}),
+      ];
+      const invest_60_40 = [
+        MockData.map((d) => {
+          return { time: (getTimestamp(d.date_time)), value: d.invest_60_40 }}),
+      ];
+      const invest_80_20 = [
+        MockData.map((d) => {
+          return { time: (getTimestamp(d.date_time)), value: d.invest_80_20 }}),
+      ];
+      const invest_day = [
+        MockData.map((d) => {
+          return { time: (getTimestamp(d.date_time)), value: d.invest_day }}),
+      ];
+
+      if (isMounted.current) {
+        setChartData({
+          btc_only: btc_only,
+          eth_only: eth_only,
+          invest_50_50: invest_50_50,
+          invest_60_40: invest_60_40,
+          invest_80_20: invest_80_20,
+          invest_day: invest_day,
+        });
+    };
   }
 
   useEffect(() => {
     loadDataCharts();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <Charts charts={charts} />;
 }
-
-
-// TODO -> IMPORT BASECHART, query data, for each row, create new serie, add to series, add to chart
